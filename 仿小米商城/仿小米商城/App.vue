d1009e43589fa84c7d3e53f7be3b00d922f342f6
coder <allisonpeng0424@qq.com>
2021-11-23 00:22:06 +0800

    uniapp

---
+++ 仿小米商城/仿小米商城/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "/common/uni.css";  //官方ui库
+	@import "/common/animate.css";  //第三方css动画库
+	@import "/common/icon.css";  //自定义图标库
+	@import "/common/zcm-main.css";  //UI基础库
+	@import "/common/common.css";  //公共样式
+</style>
---
+++ 仿小米商城/仿小米商城/REANME.md
@@ -0,0 +1,8 @@
+# 仿小米商城
+
+## 1. App.vue引入全局公共样式
+
+1. uni.css  //官方ui库
+2. animate.css  //第三方css动画库
+3. icon.css  //自定义图标库
+4. common.css  //公共样式
