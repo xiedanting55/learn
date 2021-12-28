c874fab94c800fe85496f4a0bd80c7f10f7d6bc8
coder <allisonpeng0424@qq.com>
2021-12-28 23:10:35 +0800

    iff

---
+++ IFF/shop/pages/add-edit-address/add-edit-address.vue
@@ -0,0 +1,83 @@
+<template>
+	<view class="add-edit-address">
+		<view class="item rounded mx-3 mt-1">
+			<view class="d-flex a-center border-bottom py-1">
+				<text class="text1 pl-1">收件人</text>
+				<input class="text2 pl-1" type="text" placeholder="请填写收货人姓名" />
+				<view class="row a-center j-end mr-1 row-re">
+					<image src="/static/images/maintenance-txl.png" mode="widthFix"></image>
+					<text class="pl-1">通讯录</text>
+				</view>
+			</view>
+			<view class="d-flex a-center border-bottom py-1">
+				<text class="text1 pl-1">电话号码</text>
+				<input class="text2 pl-1" type="number" placeholder="+86" />
+			</view>
+			<view class="d-flex a-center border-bottom py-1">
+				<text class="text1 pl-1">所在地区</text>
+				<text class="text2 pl-1">广东深圳市罗湖区</text>
+				<view class="d-flex a-center j-end mr-1 row-re">
+					<image src="/static/images/maintenance-dw.png" mode="widthFix"></image>
+					<text class="pl-1">定位</text>
+				</view>
+			</view>
+			<view class="d-flex row-max a-center border-bottom py-1">
+				<text class="text1 pl-1">详细地址</text>
+				<text class="text2 pl-1">草埔西吓围新村</text>
+				<view class="d-flex a-center j-end mr-1 row-re">
+					<image src="/static/images/maintenance-yy.png" mode="widthFix" class="small"></image>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isAdd: true
+			}
+		},
+		methods: {
+			
+		},
+		onLoad() {
+			uni.setNavigationBarTitle({
+				title:"新建收货地址"
+			})
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.add-edit-address {
+		.d-flex {
+			background-color: #e4e4e4;
+			&.row-max {
+				height: 102rpx;
+			}
+		}
+		text, input {
+			display: block;
+			font-size: 24rpx;
+		}
+		.text1 {
+			width: 120rpx;
+			color: #2f2f2f;
+		}
+		.text2 {
+			width: 425rpx;
+			color: #2d2d2d;
+		}
+		image {
+			width: 30rpx;
+			&.small {
+				width: 24rpx;
+			}
+		}
+		.row-re {
+			width: 115rpx;
+		}
+	}
+</style>
---
+++ IFF/shop/pages/address/address.vue
@@ -0,0 +1,77 @@
+<template>
+	<view class="address">
+		<view class="row a-center j-sb mx-3 py-3 border-bottom">
+			<view class="row-le">
+				<view class="address-head d-flex a-center">
+					<text class="main-bg-color text-white px-2 rounded-4">家</text>
+					<text>广东深圳市罗湖区</text>
+				</view>
+				<text>草埔西吓围新村</text>
+				<view class="address-info">
+					<text>孔女士</text>
+					<text class="ml-1">187****0091</text>
+				</view>
+			</view>
+			<image src="/static/images/edit.png" mode="widthFix"></image>
+		</view>
+		<view class="row a-center j-sb mx-3 py-3 border-bottom">
+			<view class="row-le">
+				<view class="address-head d-flex a-center">
+					<text class="main-bg-color text-white px-2 rounded-4">公司</text>
+					<text>广东深圳市罗湖区</text>
+				</view>
+				<text>草埔西吓围新村</text>
+				<view class="address-info">
+					<text>孔女士</text>
+					<text class="ml-1">187****0091</text>
+				</view>
+			</view>
+			<image src="/static/images/edit.png" mode="widthFix"></image>
+		</view>
+		<view class="position-fixed btn d-flex a-center j-center main-bg-color rounded-4">
+			<image src="/static/images/add.png" mode="widthFix"></image>
+			<text class="text-white font-weight ml-1">新建收获地址</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.address {
+		.row {
+			border-color: #e4e4e4;
+		}
+		image {
+			width: 23rpx;
+		}
+		text {
+			font-size: 24rpx;
+			color: #909090;
+		}
+		.btn {
+			width: 400rpx;
+			line-height: 88rpx;
+			bottom: 20px;
+			left: 50%;
+			transform: translate(-50%,0);
+			image {
+				width: 28rpx;
+			}
+			text {
+				font-size: 30rpx;
+			}
+		}
+	}
+</style>
